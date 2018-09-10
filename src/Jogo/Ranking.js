38b54f05b48917b561dbaddca6a55fb6bb795c60
Gabriel Gama <gabrielgamagga@gmail.com>
2018-09-10 19:56:36 -0400

    Organização da navegação entre telas

--- src/Jogo/Ranking.js
+++ src/Jogo/Ranking.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Container, List } from 'semantic-ui-react';
+
 import Usuario from './Usuario';
+import HeaderCustom from './HeaderCustom';
+
 
 class Ranking extends Component {
     render() {
         return (
             <div>
+                <HeaderCustom/>
                 <h2>RANKING</h2>
                 <p>Quem é o mestre???</p>
 
@@ -13,6 +17,7 @@ class Ranking extends Component {
                     <List divided verticalAlign='left'>
                         <Usuario foto='' nome='Gabriel Gama' pontos='100'/>
                         <Usuario foto='' nome='Miguel Arcanjo' pontos='85'/>
+                        <Usuario foto='' nome='Albuquerque' pontos='50'/>
                     </List>
                 </Container>
             </div>
--- src/Jogo/Resultado.js
+++ src/Jogo/Resultado.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { List, Container } from 'semantic-ui-react';
+
 import Resposta from './Resposta';
+import HeaderCustom from './HeaderCustom';
 
 const resposta = {
     id: 1,
@@ -12,6 +14,7 @@ class Resultado extends Component {
     render() {
         return (
             <div>
+                <HeaderCustom/>
                 <h2>Seus Resultados</h2>
                 <p>Confira o seu desempenho nessa categoria</p>
 
