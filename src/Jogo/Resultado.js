b256f4f4a9b65702a6dc2b604cc4bef7eff118d0
Gabriel Gama <gabrielgamagga@gmail.com>
2018-09-10 16:03:54 -0400

    cirando componente Resposta para reuso

--- src/Jogo/Resultado.js
+++ src/Jogo/Resultado.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import { List, Label } from 'semantic-ui-react';
+import { List, Container } from 'semantic-ui-react';
+import Resposta from './Resposta';
 
+const resposta = {
+    id: 1,
+    titulo: 'Primeira Pergunta',
+    alternativa: 3
+}
 
 class Resultado extends Component {
     render() {
@@ -9,33 +15,11 @@ class Resultado extends Component {
                 <h2>Seus Resultados</h2>
                 <p>Confira o seu desempenho nessa categoria</p>
 
-
-                <List divided verticalAlign='left'>
-                    <List.Item>
-                        <List.Content floated='left'>
-                            <Label>1</Label>
-                            Pergunta sobre bla bla bla
-                        </List.Content>
-                        <List.Content floated='right'>3</List.Content>
-                    </List.Item>
-
-                    <List.Item>
-                        <List.Content floated='left'>
-                            <Label>2</Label>
-                            Pergunta sobre bla bla bla
-                        </List.Content>
-                        <List.Content floated='right'>3</List.Content>
-                    </List.Item>
-
-                    <List.Item>
-                        <List.Content floated='left'>
-                            <Label>3</Label>
-                            Pergunta sobre bla bla bla
-                        </List.Content>
-                        <List.Content floated='right'>3</List.Content>
-                    </List.Item>
-
-                </List>
+                <Container>
+                    <List divided verticalAlign='left'>
+                        <Resposta resposta={resposta}/>
+                    </List>
+                </Container>
             </div>
         )
     }
