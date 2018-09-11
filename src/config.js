00b3361d94c747e7539584d4c93aff0cc38604c7
Gabriel Gama <gabrielgamagga@gmail.com>
2018-09-10 21:32:27 -0400

    Criando App no facebook e Auth Provider

---
+++ src/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+import Rebase from 're-base';
+
+
+// Initialize Firebase
+ const firebaseInfo = {
+    apiKey: "AIzaSyBE2SPuH01dxz7tPJvmTY5jx6DbQtmCC2s",
+    authDomain: "quiz-8b311.firebaseapp.com",
+    databaseURL: "https://quiz-8b311.firebaseio.com",
+    projectId: "quiz-8b311",
+    storageBucket: "quiz-8b311.appspot.com",
+    messagingSenderId: "902438624748"
+  };
+
+  const db = firebase.database(firebaseInfo);
+  const config = Rebase.createClass(db);
+
+  export const providers = {
+    'facebook' : new firebase.auth.FacebookAuthProvider,
+  }
+
+
+  export const auth = firebase.auth();
+  export default config;
